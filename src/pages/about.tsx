cc435b7413af93c033c5f044e65911ab3cc4a922
jordanctran <jordntran.t@gmail.com>
2024-03-23 19:04:07 -0400

    Added more detail to About

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Form } from "react-bootstrap";
+import { Container, ListGroup } from "react-bootstrap";
 
 
 const About = () => {
@@ -8,17 +8,23 @@ const About = () => {
 
     return (
         <Container>
-            <div className="text-center"><h1>Congrats!</h1></div>
+            <div className="text-center"><h1>Congratulations!</h1></div>
             <div className="text-center"><h4><b>You</b> have sucessfully breached the vulnerability of our site.</h4></div>
             <div className = "mt-5">
-                <div className = "py-3">
-                    <h1>Learn more below</h1>
-                </div>
-
+                <h2>Learn more below</h2>
                 <ul>
-                    <h5>What is Cybersecurity?</h5>
-                    <li><a href = "https://www.cisa.gov/news-events/news/what-cybersecurity"> What Is Cybersecurity? - Cisco </a> provides an overview of cybersecurity, which is the practice of protecting systems, networks, and programs from digital attacks.</li>
+                    <h5>What is Cybersecurity</h5>
+                    <li><a href = "https://www.cisa.gov/news-events/news/what-cybersecurity"> What Is Cybersecurity? - Cisco: </a> 
+                    provides an overview of cybersecurity, which is the practice of protecting systems, networks, and programs from digital attacks.</li>
+                    <h5>How to hack</h5>
+                    <li><a href = "https://tryhackme.com/">Tryhackme: </a> offers interactive cybersecurity training through hands-on challenges designed to teach ethical hacking skills</li>
+                    <h5>Title</h5>
+                    <li><a href = "">Example_Web_Name: </a> Example</li> 
+                    <h5>Title</h5>
+                    <li><a href = "">Example_Web_Name: </a> Example</li> 
                 </ul>
+
+                
             </div> 
         </Container>
     )
