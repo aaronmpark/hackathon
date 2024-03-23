4503c29a67a2284b027fb9f7e465a81b226dfe11
jordanctran <jordntran.t@gmail.com>
2024-03-23 17:49:18 -0400

    Expanded on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Stack } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 
 
 const About = () => {
     const navigate = useNavigate();
 
     return (
-        <Stack gap={3}>
-            <div className="p2"> <h1>About</h1></div>
-            <div className="p2"> <p>This is very informative</p></div>
-        </Stack>
+        <Container>
+            <div className="text-center"><h1>Congrats!</h1></div>
+            <div className="text-center"><h4><b>You</b> have sucessfully breached the vulnerability of our site.</h4></div>
+            <div className = "mt-5">
+                <div className = "py-3">
+                    <h1>Learn more below</h1>
+                </div>
+
+                <ul>
+                    <h5>What is Cybersecurity?</h5>
+                    <li><a href = "https://www.cisa.gov/news-events/news/what-cybersecurity"> What Is Cybersecurity? - Cisco </a> provides an overview of cybersecurity, which is the practice of protecting systems, networks, and programs from digital attacks.</li>
+                </ul>
+            </div> 
+        </Container>
     )
 }
 
